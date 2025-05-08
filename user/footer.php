5bd819aef94877646bf577496017dcfb577a6079
SINGWENJIE <1221207951@student.mmu.edu.my>
2025-05-09 03:41:44 +0800

    footer done

---
+++ user/footer.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>GOGO | Shop Conveniently</title>
+    <link rel="icon" type="image" href="../../image/GOGO.png">
+    <link rel="stylesheet" href="../user_assets/css/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=search" />
+</head>
+
+<body>
+<div class="footer-nav">
+    <div class="footer-column">
+        <h4>Our Helpline</h4>
+        <ul>
+            <li><a href="">MR.SING</a></li>
+            <li><a href="">MR.PIOW</a></li>
+            <li><a href="">MR.CHEW</a></li>
+        </ul>
+    </div>
+
+    <div class="footer-column">
+        <h4>News & Media</h4>
+        <ul>
+            <li><a href="#">Press Release</a></li>
+            <li><a href="#">News Article</a></li>
+        </ul>
+    </div>
+
+    <div class="footer-column">
+        <h4>Policies</h4>
+        <ul>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="../TermsConditions/TermsConditions.html">Terms & Conditions</a></li>
+            <li><a href="#">Anti Bribery Policies</a></li>
+            <li><a href="#">Electrical Policy</a></li>
+        </ul>
+    </div>
+
+    <div class="footer-column">
+        <h4>&nbsp;</h4>
+        <ul>
+            <li><a href="#">Return Policy</a></li>
+            <li><a href="#">Product Policy</a></li>
+            <li><a href="#">Halal Statement</a></li>
+        </ul>
+    </div>
+
+    <div class="footer-column">
+        <ul>
+            <li>
+                <a href="https://www.instagram.com/cheeew.05?igsh=MTBvcTQ5MXR0emNidQ%3D%3D&utm_source=qr">
+                    <i class="fab fa-instagram" style="font-size: 30px; margin-top: 75px;"></i>
+                </a>
+            </li>
+            <li>&copy; GOGO_SUPERMARKET</li>
+        </ul>
+    </div>
+</div>
+</body>
+
+</html>
