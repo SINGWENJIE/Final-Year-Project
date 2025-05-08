043355b3effdf2ca2055a347c9c62dd06dfbe186
SINGWENJIE <1221207951@student.mmu.edu.my>
2025-05-09 04:42:04 +0800

    header done

---
+++ user/header.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gogo_supermarket";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GOGO- Supermarket</title>
+    <link rel="stylesheet" href="../user_assets/css/header.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
+<body>
+<header>
+    <div class="container column-layout">
+        <div class="left-section">
+            <h1>            
+                <a href="MainPage.php"><img src="../image/gogoname.png" alt="GOGO Logo"></a>
+            </h1>
+
+            <nav class="sub-nav">
+                <ul class="nav-links">
+                    <li><a href="MainPage.html">Menu</a></li>
+                    <li><a href="../AboutUs/AboutUs.html">About GOGO</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <nav class="main-nav">
+            <a href="product_list.php"><i class="fas fa-store"></i> Products</a>
+            <a href="#"><i class="fas fa-heart"></i> Wishlist</a>
+            <a href="Cart.php"><i class="fas fa-shopping-cart"></i> Cart</a>
+            <span class="user-info">
+                <a href="Profile/Profile.php"><i class="fas fa-user"></i> <?php echo htmlspecialchars($_SESSION['user_name']); ?></a>
+                <a href="logout.php" class="logout-btn">Logout</a>
+            </span>
+        </nav>
+    </div>
+</header>
+
+
+</body>
+</html>
