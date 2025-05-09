7d3fb66a85763d781a41e8527cebed9bbb0e2b27
CHEW WEI XIN <1221209540@soffice.mmu.edu.my>
2025-05-09 19:09:33 +0800

    123

--- user/Cart.php
+++ user/Cart.php
@@ -58,7 +58,6 @@ $is_cart_empty = empty($cart_items);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shopping Cart - Supermarket</title>
-    <link rel="stylesheet" href="../user_assets/css/header.css">
     <link rel="stylesheet" href="../user_assets/css/cart.css">
     <link rel="stylesheet" href="../user_assets/css/footer.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
